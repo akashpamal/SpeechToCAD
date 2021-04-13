052232f874e33274dce481de352a6e7bba038820
Akash Pamal <akashpamal@gmail.com>
2021-04-13 09:44:38 -0400

    realtime audio works when run from the terminal

---
+++ speech_to_text_sample.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+import pyaudio
+
+init_rec = sr.Recognizer()
+print("Let's speak!!")
+with sr.Microphone() as source:
+    audio_data = init_rec.record(source, duration=5)
+    print("Recognizing your text.............")
+    text = init_rec.recognize_google(audio_data)
+    print(text)
+
+"""
+If the above script throws a "speech_recognition.UnknownValueError", run the script from the "Terminal" application on Mac, rather than the integrated terminal in VSCode.
+"""
